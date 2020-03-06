41e2a3ebbcefb124ee3204775bc8111737a9f8fe
Tiernan Cahill <tjcahill@bu.edu>
2020-03-05 20:29:04 -0500

    Cleanup code structure in GSR cleaning script

--- gsr_clean.R
+++ gsr_clean.R
@@ -1,27 +1,49 @@
 library(readr)
 library(dplyr)
 
-w1_gsr <- read_delim("data/W1_GSRSummaryScore.tsv", 
-                     "\t", escape_double = FALSE, col_types = cols(Age = col_integer(), 
-                                                                   `Duration (sec)` = col_skip(), Gender = col_factor(levels = c("FEMALE", 
-                                                                                                                                 "MALE")), 
-                                                                   Group = col_skip(), 
-                                                                   HasPeak = col_logical(), `Peak Detection Name` = col_skip(), 
-                                                                   PeakCount = col_integer(), RespondentName = col_character(), 
-                                                                   Segmentation = col_skip(), `Start-Stop (sec)` = col_skip(), 
-                                                                   `StimulusName/Scene` = col_character(), 
-                                                                   StudyName = col_skip()), trim_ws = TRUE)
+w1_gsr <- read_delim(
+  "data/W1_GSRSummaryScore.tsv",
+  "\t",
+  escape_double = FALSE,
+  col_types = cols(
+    Age = col_integer(),
+    `Duration (sec)` = col_skip(),
+    Gender = col_factor(levels = c("FEMALE",
+                                   "MALE")),
+    Group = col_skip(),
+    HasPeak = col_logical(),
+    `Peak Detection Name` = col_skip(),
+    PeakCount = col_integer(),
+    RespondentName = col_character(),
+    Segmentation = col_skip(),
+    `Start-Stop (sec)` = col_skip(),
+    `StimulusName/Scene` = col_character(),
+    StudyName = col_skip()
+  ),
+  trim_ws = TRUE
+)
 
-w2_gsr <- read_delim("data/W2_GSRSummaryScore.tsv", 
-                     "\t", escape_double = FALSE, col_types = cols(Age = col_integer(), 
-                                                                   `Duration (sec)` = col_skip(), Gender = col_factor(levels = c("FEMALE", 
-                                                                                                                                 "MALE")), 
-                                                                   Group = col_skip(), 
-                                                                   HasPeak = col_logical(), `Peak Detection Name` = col_skip(), 
-                                                                   PeakCount = col_integer(), RespondentName = col_character(), 
-                                                                   Segmentation = col_skip(), `Start-Stop (sec)` = col_skip(), 
-                                                                   `StimulusName/Scene` = col_character(), 
-                                                                   StudyName = col_skip()), trim_ws = TRUE)
+w2_gsr <- read_delim(
+  "data/W2_GSRSummaryScore.tsv",
+  "\t",
+  escape_double = FALSE,
+  col_types = cols(
+    Age = col_integer(),
+    `Duration (sec)` = col_skip(),
+    Gender = col_factor(levels = c("FEMALE",
+                                   "MALE")),
+    Group = col_skip(),
+    HasPeak = col_logical(),
+    `Peak Detection Name` = col_skip(),
+    PeakCount = col_integer(),
+    RespondentName = col_character(),
+    Segmentation = col_skip(),
+    `Start-Stop (sec)` = col_skip(),
+    `StimulusName/Scene` = col_character(),
+    StudyName = col_skip()
+  ),
+  trim_ws = TRUE
+)
 
 # Merge waves of data
 w1_gsr$Wave <- 1
@@ -30,7 +52,15 @@ w2_gsr$Wave <- 2
 gsr <- w1_gsr %>% bind_rows(w2_gsr)
 
 # Clean column names
-colnames(gsr) <- c("scene", "respondent", "gender", "age", "hasPeak", "peakCount", "peaksPerMin", "wave")
+colnames(gsr) <-
+  c("scene",
+    "respondent",
+    "gender",
+    "age",
+    "hasPeak",
+    "peakCount",
+    "peaksPerMin",
+    "wave")
 
 # Remove non-stimulus segments from data
 gsr <- gsr %>% filter(!grepl('Survey', scene))
