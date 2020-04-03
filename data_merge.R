a0eb01c93ed7eaf03d07701044e194eff9e81982
Tiernan Cahill <tjcahill@bu.edu>
2020-04-02 20:07:37 -0400

    Create data_merge.R

---
+++ data_merge.R
@@ -0,0 +1,6 @@
+################################################################################
+# DATA_MERGE.R                                                                 #
+# ============================================================================ #
+# This script takes the cleaned output from the *_clean.R scripts and merges   #
+# them together into a single master dataset for all subsequent analysis.      #
+################################################################################
