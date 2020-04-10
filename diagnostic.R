edecb4ca64275a159ddb0587b3328b1ff790732c
Tiernan Cahill <tjcahill@bu.edu>
2020-04-10 00:57:26 -0400

    Create analysis script files

---
+++ diagnostic.R
@@ -0,0 +1,7 @@
+################################################################################
+# DIAGNOSTIC.R                                                                 #
+# ============================================================================ #
+# This script takes the cleaned output from the DATA_MERGE script and runs     #
+# diagnostics and outputs univariate stats for the first part of the analysis, #
+# setting the groundwork for later hypothesis tests.                           #
+################################################################################
