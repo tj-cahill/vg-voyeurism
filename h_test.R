52b24715769f61c5c3cc0bfec239d4e3639c71b8
Tiernan Cahill <tjcahill@bu.edu>
2020-04-10 01:13:36 -0400

    Converted analysis scripts to RMD notebooks
    Using R Notebooks will allow for easier annotation and reporting of results.

--- h_test.R
+++
@@ -1,7 +0,0 @@
-################################################################################
-# H_TEST.R                                                                     #
-# ============================================================================ #
-# This script takes the cleaned output from the DATA_MERGE script and runs     #
-# hypothesis tests and inductive stats for the second part of the analysis,    #
-# following DIAGNOSTIC.                                                        #
-################################################################################
